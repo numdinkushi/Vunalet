2e62be12e28fdeb4d5c91e6a52436d9e14599854
Kushi Numdin <numdinkushi@gmail.com>
2025-08-05 07:26:20 +0100

    fixed middledware error

--- middleware.ts
+++ middleware.ts
@@ -20,7 +20,7 @@ export default clerkMiddleware(async (auth, req) => {
 // Match all pages and API routes, but skip Next.js internals and static assets
 export const config = {
     matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|mp4|webm|ogg|mov|avi|mkv)).*)',
         '/(api|trpc)(.*)',
     ],
 }; 
